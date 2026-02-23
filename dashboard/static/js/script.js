b7a0c4765badf02977a03bbe0793a98cfb4d923d
arifrg1704-cpu <arifrg1704@gmail.com>
2026-02-23 11:54:46 +0700

    UI: Hide CCTV titles in Display View to maximize video size

--- dashboard/static/js/script.js
+++ dashboard/static/js/script.js
@@ -684,19 +684,6 @@ function renderDisplayView() {
                     </button>
                 </div>
             </div>
-            <div class="cctv-info">
-                <h3 class="cctv-title">${escapeHtml(cctv.nama_lokasi)}</h3>
-                <div class="cctv-meta">
-                    <span class="cctv-location">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle></svg>
-                        ${escapeHtml(cctv.kecamatan)}
-                    </span>
-                    <span class="cctv-status ${statusClass}">
-                        <span class="status-dot"></span>
-                        ${statusText}
-                    </span>
-                </div>
-            </div>
         `;
         grid.appendChild(card);
     });
